ce27a2e357aff7ff717c6745c15f9abc6dfaba20
Salmo Mascarenhas <salmo.cruz@gmail.com>
2023-03-03 23:35:13 -0300

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,8 @@ module.exports = {
     testEnvironment: 'node',
     transform: {
         '.+\\.ts$': 'ts-jest',
-    }
+    },
+    moduleNameMapper: {
+        '@/(.*)': '<rootDir>/src/$1',
+    },
 }
