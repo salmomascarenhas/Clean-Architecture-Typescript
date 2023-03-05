f9fc7e9ed4082647568afcb771b4578e1fdb5bd0
Salmo Mascarenhas <salmo.cruz@gmail.com>
2023-03-05 18:41:44 -0300

    ensure LocalSavePurchases insert cache with correct key

--- src/data/usecases/save-purchases/local-save-purchases.ts
+++ src/data/usecases/save-purchases/local-save-purchases.ts
@@ -3,11 +3,8 @@ export class LocalSavePurchases {
 
     constructor(private readonly cacheStore: CacheStore) { }
 
-    async init(): Promise<void> {
-        return new Promise(resolve => resolve())
-    }
-
     async save(): Promise<void> {
         this.cacheStore.delete('purchases')
+        this.cacheStore.insert('purchases')
     }
 }
