5b0ed186ec9bef97d9055816dddccc236ea9514d
Salmo Mascarenhas <salmo.cruz@gmail.com>
2023-03-05 19:21:01 -0300

    ensure LocalSavePurchases local save purchases throws if CachaStore.insert throws

--- src/data/usecases/save-purchases/local-save-purchases.spec.ts
+++ src/data/usecases/save-purchases/local-save-purchases.spec.ts
@@ -22,7 +22,10 @@ class CacheStoreSpy implements CacheStore {
 
     simulateDeleteError(): void {
         jest.spyOn(CacheStoreSpy.prototype, 'delete').mockImplementationOnce(() => { throw new Error() })
+    }
 
+    simulateInsertError(): void {
+        jest.spyOn(CacheStoreSpy.prototype, 'insert').mockImplementationOnce(() => { throw new Error() })
     }
 }
 
@@ -84,5 +87,13 @@ describe('LocalSavePurchases', () => {
         expect(cacheStore.insertCallsCount).toBe(1)
         expect(cacheStore.insertKey).toBe('purchases')
         expect(cacheStore.insertValues).toEqual(purchases)
+    })
+
+    test('Should throw if insert throws', () => {
+        const { cacheStore, sut } = makeSut()
+        cacheStore.simulateInsertError()
+        const purchases = mockPurchases()
+        const promise = sut.save(purchases)
+        expect(promise).rejects.toThrow()
     })
 })
