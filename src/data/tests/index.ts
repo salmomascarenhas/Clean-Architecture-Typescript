8a288ca2013b9a9f56abbbb6b7304207cae65cdb
Salmo Mascarenhas <salmo.cruz@gmail.com>
2023-03-05 23:28:34 -0300

    change asserts to use array to ensure order and quantity of calls

---
+++ src/data/tests/index.ts
@@ -0,0 +1,3 @@
+export * from './mock-cache'
+export * from './mock-purchases'
+
---
+++ src/data/tests/mock-cache.ts
@@ -0,0 +1,41 @@
+import { CacheStore } from '@/data/protocols/cache'
+import { SavePurchases } from "@/domain/usecases"
+
+export class CacheStoreSpy implements CacheStore {
+    messages: Array<CacheStoreSpy.Message> = []
+    deleteKey: string
+    insertKey: string
+    insertValues: Array<SavePurchases.Params> = []
+
+    delete(key: string): void {
+        this.messages.push(CacheStoreSpy.Message.delete)
+        this.deleteKey = key
+    }
+
+    insert(key: string, value: any): void {
+        this.messages.push(CacheStoreSpy.Message.insert)
+        this.insertKey = key
+        this.insertValues = value
+    }
+
+    simulateDeleteError(): void {
+        jest.spyOn(CacheStoreSpy.prototype, 'delete').mockImplementationOnce(() => {
+            this.messages.push(CacheStoreSpy.Message.delete)
+            throw new Error()
+        })
+    }
+
+    simulateInsertError(): void {
+        jest.spyOn(CacheStoreSpy.prototype, 'insert').mockImplementationOnce(() => {
+            this.messages.push(CacheStoreSpy.Message.insert)
+            throw new Error()
+        })
+    }
+}
+
+export namespace CacheStoreSpy {
+    export enum Message {
+        delete = 'delete',
+        insert = 'insert'
+    }
+}
